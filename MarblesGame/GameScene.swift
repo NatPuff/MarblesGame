1562ff6316639cac1033d927685fb3e2917385e8
NatPuff <cesarp102104@gmail.com>
2021-12-01 07:32:25 -0600

    done
    done

--- MarblesGame/GameScene.swift
+++ MarblesGame/GameScene.swift
@@ -8,81 +8,126 @@
 import SpriteKit
 import GameplayKit
 
+
+
+class Marble: SKSpriteNode { }
+
+
 class GameScene: SKScene {
+
+    var marbles = ["marbleBlue", "marbleGreen", "marblePurple", "marbleRed", "marbleYellow", "marbleGrey"]
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    let scoreLabel = SKLabelNode(fontNamed: "HelveticaNeue-Thin")
+    var matchedMarbles = Set<Marble>()
+    
+    var score = 0 {
+        didSet {
+            let formatter = NumberFormatter()
+            formatter.numberStyle = .decimal
+            let formattedScore = formatter.string(from: score as NSNumber) ?? "0"
+            
+            scoreLabel.text = "SCORE: \(formattedScore)"
+        }
+    }
     
     override func didMove(to view: SKView) {
+        let marble = SKSpriteNode(imageNamed: "marbleBlue")
+        let marbleRadius = marble.frame.width / 2.0
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        scoreLabel.fontSize = 72
+        scoreLabel.position = CGPoint(x: 20, y: 20)
+        scoreLabel.text = "SCORE: 0"
+        scoreLabel.zPosition = 100
+        scoreLabel.horizontalAlignmentMode = .left
+        addChild(scoreLabel)
+        
+        
+        for i in stride(from: marbleRadius, to: view.bounds.width - marbleRadius, by: marble.frame.width) {
+            for j in stride(from: 100, to: view.bounds.height, by: marble.frame.height) {
+                let marbleType = marbles.randomElement()!
+                let marble = Marble(imageNamed: marbleType)
+                marble.position = CGPoint(x: i, y: j)
+                marble.name = marbleType
+               
+                
+                marble.physicsBody = SKPhysicsBody(circleOfRadius: marbleRadius)
+                marble.physicsBody?.allowsRotation = false
+                marble.physicsBody?.friction = 0
+                marble.physicsBody?.restitution = 0
+                
+                addChild(marble)
+            }
         }
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame.inset(by: UIEdgeInsets(top: 100, left: 0, bottom: 0, right: 0)))
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+    }
+    
+//    func getMatches(from node: Marble) {
+//        for body in node.physicsBody!.allContactedBodies() {
+//            guard let marble = body.node as? Marble else { continue }
+//            guard marble.name == node.name else { continue }
+//
+//            if !matchedMarbles.contains(marble) {
+//                matchedMarbles.insert(marble)
+//                getMatches(from: marble)
+//            }
+//        }
+//    }
+    
+    
+    func getMatches(from StartMarble: Marble) {
+        let matchWidth = StartMarble.frame.width * StartMarble.frame.width
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+        for node in children {
+            guard let marble = node as?  Marble else {continue}
+            guard marble.name == StartMarble.name else {continue}
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+            let dist = distance(from: StartMarble, to: marble)
+            
+            guard dist < matchWidth else {continue}
+            
+            if !matchedMarbles.contains(marble) {
+                matchedMarbles.insert(marble)
+                getMatches(from: marble)
+            }
         }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
+        super.touchesEnded(touches, with: event)
+        
+        guard let position = touches.first?.location(in: self) else {
+            return }
+            guard let tappedMarble = nodes(at: position).first(where: {$0 is Marble}) as? Marble else { return }
+            
+                matchedMarbles.removeAll(keepingCapacity: true)
+                
+                getMatches(from: tappedMarble)
+        if matchedMarbles.count >= 3 {
+            score += Int(pow(2, Double(min(matchedMarbles.count, 8))))
+            
+            for marble in matchedMarbles{
+                if let particles = SKEmitterNode(fileNamed: "waterblast") {
+                    particles.position = marble.position
+                    addChild(particles)
+                    
+                    let removedAfterMatched =
+                    SKAction.sequence([SKAction.wait(forDuration: 3),
+                                       SKAction.removeFromParent()])
+                    particles.run(removedAfterMatched)
+                }
+                
+                marble.removeFromParent()
+            }
+        }
+            }
     
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
     
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+    func distance(from: Marble, to: Marble) -> CGFloat {
+        return (from.position.x - to.position.x) * (from.position.x - to.position.x) + (from.position.y - to.position.y) * (from.position.y - to.position.y)
     }
-}
+        }
+
